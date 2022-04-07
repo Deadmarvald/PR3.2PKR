7d254c7634802b9c7f23413a25c63bfb1477db9d
unknown <deadmarvald@gmail.com>
2022-04-07 07:47:09 +0300

    Лабораторна робота №3.2 ПКР

---
+++ UT3.2PKR/UT3.2PKR.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR3.2PKR/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UT32PKR
+{
+	TEST_CLASS(UT32PKR)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			int N = 3;
+			Student* s = new Student[N];
+
+			s[0].prog = 5;
+			s[1].prog = 5;
+			s[2].prog = 5;
+			s[0].kurs = 1;
+			s[1].kurs = 1;
+			s[2].kurs = 1;
+			s[0].secondname = "1";
+			s[0].secondname = "1";
+			s[0].secondname = "1";
+
+			t = BinSearch(s, N, 5, 1, "1");
+
+			Assert::AreEqual(t, -1.);
+		}
+	};
+}
